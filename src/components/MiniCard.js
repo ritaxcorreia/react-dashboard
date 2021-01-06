560edfde4462bef2add4cc453a63bb169435da14
Rita <rita@ritacorreia.co>
2021-01-06 12:10:47 +0000

    Finished Mini Card component

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -3,24 +3,26 @@ import "./MiniCard.css";
 function MiniCard(props) {
 	return (
 		<article className="mini-container">
-			<div>
+			<header className="flex-header">
 				<h2 className="title">{props.title}</h2>
 				<img
 					className="mini-icon"
 					src={`./images/icon-${props.service}.svg`}
 					alt={`${props.service} icon`}
 				/>
-			</div>
-			<div className="overview-stats">
+			</header>
+			<div className="flex-stats">
 				<h3 className="today-count">{props.todayCount}</h3>
-				<img
-					className="mini-arrow"
-					src={`./images/icon-${props.movement}.svg`}
-					alt={`${props.movement} arrow`}
-				/>
-				<p className={`percentage ${props.movement}`}>
-					{props.percentage}%
-				</p>
+				<div>
+					<img
+						className="mini-arrow"
+						src={`./images/icon-${props.movement}.svg`}
+						alt={`${props.movement} arrow`}
+					/>
+					<p className={`percentage ${props.movement}`}>
+						{props.percentage}%
+					</p>
+				</div>
 			</div>
 		</article>
 	);
