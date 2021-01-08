b7c7afc6af48e243bb4ad53be900e5ce799f57c4
Rita <rita@ritacorreia.co>
2021-01-08 10:49:02 +0000

    Aligning dark mode toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
 import "./Toggle.css";
 
 function Toggle() {
-	return <input className="toggle" type="checkbox"></input>;
+	return (
+		<div className="toggle-group">
+			<h2 className="dark-mode-text">Dark Mode</h2>
+			<input className="toggle" type="checkbox"></input>
+		</div>
+	);
 }
 
 export default Toggle;
